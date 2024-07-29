8cee12103a45161e15b1e7b98fb4f8935ce6e516
Absurdities Media LLC <absurditiesmediallc@gmail.com>
2024-07-28 20:25:49 -0400

    add absurditiesmedia link

--- astro.config.ts
+++ astro.config.ts
@@ -4,17 +4,24 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site:
+  site: 
     process.env.VERCEL_ENV === 'production'
       ? 'https://infostrikes.com/'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://infostrikes.com/',
+    
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
+ 
   },
+  redirects: {
+   '/blog': '/news',
+   '/blog/[...slug]': '/news/[...slug]' 
+  },
+  devToolbar: {enabled: true}, 
 });
---
+++ src/site_config.ts
@@ -0,0 +1,6 @@
+export const SITE: Site = {
+  name: "infoSTRIKES!",
+  description:"All the news the deep state hides in their hidden tunnels underground.",
+  canonicalURL:"https://infostrikes.com",
+  colors: ["#42ff5b","#fffb29","#3d6aff","#ff57ff","#ff1f4b","#52ffeb","#6aff3d","#7105ff","#ff05bc","#ff0565"],
+};
